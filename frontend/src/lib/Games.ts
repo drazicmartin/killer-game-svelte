f092162d10307fb48bac57051aa3b4ac1f0fc5e3
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2023-09-22 16:48:17 +0200

    feat: game logic kill, init ...

--- frontend/src/lib/Games.ts
+++ frontend/src/lib/Games.ts
@@ -1,6 +1,5 @@
 import type { SupabaseClient } from "@supabase/supabase-js";
 import { fail } from "@sveltejs/kit";
-import { each } from "svelte/internal";
 
 export function GetGameStatusColor(game) {
     if (game?.is_finish) return "red"
@@ -8,8 +7,16 @@ export function GetGameStatusColor(game) {
     return "orange"
 }
 
+async function resetPlayerScore(supabase: SupabaseClient, game_id: number){
+    await supabase
+        .from("players")
+        .update({score: 0})
+        .eq("game_id", game_id);
+}
+
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+    let array_copy = [...array];
+    let currentIndex = array_copy.length,  randomIndex;
   
     // While there remain elements to shuffle.
     while (currentIndex > 0) {
@@ -19,14 +26,65 @@ function shuffle(array) {
       currentIndex--;
   
       // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+      [array_copy[currentIndex], array_copy[randomIndex]] = [
+        array_copy[randomIndex], array_copy[currentIndex]];
     }
   
-    return array;
+    return array_copy;
   }
 
-export async function InitGameRotation(supabase: SupabaseClient, game_id: number){
+export async function ShuffleGameState(supabase: SupabaseClient, game_id: number){
+        const { data: game, error: e_g } = await supabase
+        .from("games")
+        .select("*")
+        .eq("id", game_id)
+        .single();
+
+    if (game.is_started || game.is_finish){
+        return fail(405, {message: "game has not staretd yet or is already finsish"});
+    }
+
+    const { data: alive_players, error: e_p } = await supabase
+                        .from("players")
+                        .select()
+                        .eq("game_id", game_id)
+                        .eq("is_dead", false);
+
+    const { data: missions, error: e_m } = await supabase
+        .from("missions")
+        .select("*")
+        .eq("game_id", game_id);
+    
+    let state = game.state;
+
+    let remaining_mission = shuffle(missions);
+    let alive_players_shuffled = shuffle(alive_players);
+
+    alive_players_shuffled.forEach((player, index, alive_players) => {
+        if (remaining_mission.length <= 2){
+            remaining_mission = shuffle(missions);
+        }
+
+        let target_index = (index + 1) % alive_players.length;
+        let target_name = alive_players[target_index].name;
+        let target_id = alive_players[target_index].user_id;
+        let mission = remaining_mission.pop();
+        let desc = mission.description;
+    
+        state.loop[player.user_id].mission = desc;
+        state.loop[player.user_id].target_id = target_id;
+        state.loop[player.user_id].target_name = target_name;
+    });
+
+    await updateGameState(supabase, game_id, state);
+
+    return {
+        success: true,
+        message: "Successfully Shuffle Game State"
+    }
+}
+
+export async function InitGameState(supabase: SupabaseClient, game_id: number){
     const { data: game, error: e_g } = await supabase
                         .from("games")
                         .select("*")
@@ -34,58 +92,116 @@ export async function InitGameRotation(supabase: SupabaseClient, game_id: number
                         .single();
 
     if (game.is_started || game.is_finish){
-        fail(405, {error: "game already started or finished"});
+        return fail(405, {message: "game already started or finished"});
     }
 
     const { data: players, error: e_p } = await supabase
                         .from("players")
-                        .select("*")
+                        .select()
                         .eq("game_id", game_id);
     
     const { data: missions, error: e_m } = await supabase
                         .from("missions")
-                        .select("*")
+                        .select()
                         .eq("game_id", game_id);
 
-    let state = {};
-    state['loop'] = {}
+    let state = {
+        loop: {}
+    };
     state['#players'] = 0;
+    state['#alive_players'] = 0;
 
-    let remaining_mission = shuffle(missions)
-    let players_shuffled = shuffle(players)
+    let remaining_mission = shuffle(missions);
+    let players_shuffled = shuffle(players);
 
     players_shuffled.forEach((player, index, players) => {
         if (remaining_mission.length <= 2){
             remaining_mission = shuffle(missions);
         }
 
-        let target_1_index = (index + 1) % players.length;
-        let target_2_index = (index + 2) % players.length;
-        let target_1_name = players[target_1_index].name;
-        let target_2_name = players[target_2_index].name;
+        let target_index = (index + 1) % players.length;
+        let target_name = players[target_index].name;
+        let target_id =  players[target_index].user_id;
+        let mission = remaining_mission.pop();
+        let desc = mission.description;
+
+        let player_mission = {
+            target_name: target_name,
+            target_id: target_id,
+            mission: desc
+        };
     
-        let player_missions = {
-            m1: {
-                target: target_1_name,
-                // target_id: players[target_1_index].user_id,
-                mission: remaining_mission.pop().description
-            },
-            m2: {
-                target: target_2_name,
-                // target_id: players[target_2_index].user_id,
-                mission: remaining_mission.pop().description
-            }
-        }
-    
-        state['loop'][player.user_id] = player_missions
-    
-    
-        state['#players'] += 1    
+        state.loop[player.user_id] = player_mission;
+        state['#players'] += 1;
+        state['#alive_players'] += 1;
     });
 
-    console.log(state);
-    const { error } = await supabase
-        .from('games')
-        .update({ state: state })
+    await updateGameState(supabase, game_id, state);
+    await resetPlayerScore(supabase, game_id);
+
+    return {
+        success: true,
+        message: "Successfully Reset Game State"
+    }
+}
+
+export async function fetchGameState(supabase: SupabaseClient, game_id: number){
+    let {data, error} = await supabase
+        .from("games")
+        .select("state")
+        .eq("id", game_id)
+        .single();
+    return data;
+}
+
+export async function updateGameState(supabase: SupabaseClient, game_id: number, state: Object){
+    let {data, error} = await supabase
+        .from("games")
+        .update({state: state})
         .eq("id", game_id);
+}
+
+export async function setPlayerDead(supabase: SupabaseClient, game_id: number, killed_player_id: string) {
+    let {data, error} = await supabase
+        .from("players")
+        .update({is_dead: true})
+        .eq("game_id", game_id)
+        .eq("user_id", killed_player_id);
+}
+
+export async function addScore(supabase: SupabaseClient, game_id: number, player_id: string, added_score: number = 1) {
+    let {data: {score}, error} = await supabase
+        .from("players")
+        .select("score")
+        .eq("game_id", game_id)
+        .eq("user_id", player_id)
+        .single();
+
+    await supabase
+        .from("players")
+        .update({score: score + added_score})
+        .eq("game_id", game_id)
+        .eq("user_id", player_id);
+}
+
+export async function killLogic(supabase: SupabaseClient, game_id: number, killed_player_id: string, killer_player_id: string) {
+    let { state } = await fetchGameState(supabase, game_id);
+
+    addScore(supabase, game_id, killer_player_id);
+
+    setPlayerDead(supabase, game_id, killed_player_id);
+
+    let kill_history = [];
+    if ('kill_history' in state.loop[killer_player_id]){
+        kill_history = state.loop[killer_player_id].kill_history;
+    }
+    kill_history.push(state.loop[killer_player_id].target_name);
+    state.loop[killer_player_id] = state.loop[killed_player_id];
+    state.loop[killer_player_id].kill_history = kill_history;
+    
+    state.loop[killed_player_id].killed_by_id = killer_player_id;
+    state.loop[killed_player_id].is_dead = true;
+    state['#alive_players'] -= 1;
+
+    await updateGameState(supabase, game_id, state);
 }
