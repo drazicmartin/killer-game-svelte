5001cc50f9d2434ce87711cfb5fad2300f67809e
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2023-10-01 18:26:04 +0200

    fix: restrict access to state when not started

--- frontend/src/routes/game/[game_id]/state/+page.ts
+++ frontend/src/routes/game/[game_id]/state/+page.ts
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit';
 export const load = async ({ parent, params }) => {
     let {is_admin, game, self_player} = await parent();
 
-    if (!is_admin || !self_player.is_dead){
+    if (!is_admin && (!self_player.is_dead || !game.is_started)){
       throw redirect(308, `/game/${game.id}`);
     }
   }
